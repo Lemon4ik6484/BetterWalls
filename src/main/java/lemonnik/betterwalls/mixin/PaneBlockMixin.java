dbfdd9bc68fec968bd2ee82626225801f16300e7
Lemonnik6484 <LemonGamer6484@gmail.com>
2024-07-08 14:43:34 +0200

    Glass pane connection & tall walls

--- src/main/java/lemonnik/betterwalls/mixin/PaneBlockMixin.java
+++ src/main/java/lemonnik/betterwalls/mixin/PaneBlockMixin.java
@@ -2,22 +2,17 @@ package lemonnik.betterwalls.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.FenceBlock;
 import net.minecraft.block.PaneBlock;
 import net.minecraft.registry.tag.BlockTags;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
-
-@Mixin(FenceBlock.class)
-public class FenceBlockMixin {
-    @ModifyReturnValue(method = "canConnect", at = @At("RETURN"))
+@Mixin(PaneBlock.class)
+public class PaneBlockMixin {
+    @ModifyReturnValue(method = "connectsTo", at = @At("RETURN"))
     private boolean doConnectFences(boolean original, @Local BlockState state) {
-        Block block = state.getBlock();
-        return original ? true : state.isIn(BlockTags.WALLS)
-                || state.isIn(BlockTags.FENCES)
-                || block instanceof PaneBlock;
+        return original ? true : state.isIn(BlockTags.FENCES) || state.isIn(BlockTags.FENCE_GATES);
     }
 }
+
--- src/main/java/lemonnik/betterwalls/mixin/WallBlockMixin.java
+++ src/main/java/lemonnik/betterwalls/mixin/WallBlockMixin.java
@@ -2,16 +2,41 @@ package lemonnik.betterwalls.mixin;
 
 import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.PaneBlock;
 import net.minecraft.block.WallBlock;
 import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.util.BlockRotation;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(WallBlock.class)
-public class WallBlockMixin {
+public abstract class WallBlockMixin {
+    @Shadow public abstract BlockState rotate(BlockState state, BlockRotation rotation);
+
+    private static boolean beTall = false;
+
     @ModifyReturnValue(method = "shouldConnectTo", at = @At("RETURN"))
     private boolean doConnectFences(boolean original, @Local BlockState state) {
-        return original ? true : state.isIn(BlockTags.FENCES);
+        beTall = false;
+        if (!original) {
+            Block block = state.getBlock();
+            if (state.isIn(BlockTags.FENCES)) {
+                beTall = true;
+                return true;
+            }
+        }
+        return original;
+    }
+
+    @ModifyReturnValue(method = "shouldUseTallShape", at = @At("RETURN"))
+    private static boolean doTallWall(boolean original) {
+        if (beTall) {
+            return true;
+        } else {
+            return original;
+        }
     }
 }
